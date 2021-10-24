a73776ce0f380a9a3ad9296eba09046af157d72a
akprinciple <55606083+akprinciple@users.noreply.github.com>
2021-10-24 03:29:49 +0100

    Netwalkers.com.ng

--- js/jscript.js
+++ js/jscript.js
@@ -2,3 +2,29 @@ function buttonClick() {
 	document.getElementById('btn').style.backgroundColor='#ef939a';
 	
 }
+function display() {
+var x, y, z;
+	 x = document.getElementById('dropdown');
+		x.style.display="block";
+	document.getElementById('first').style.display="none";
+	document.getElementById('second').style.display="block";
+}
+function hide() {
+	
+	document.getElementById('dropdown').style.display="none";
+	document.getElementById('first').style.display="block";
+	document.getElementById('second').style.display="none";
+}
+function slidein() {
+var x, y, z;
+	 x = document.getElementById('position');
+		x.style.display="block";
+	document.getElementById('firstsam').style.display="none";
+	document.getElementById('secondsam').style.display="block";
+}
+function slideout() {
+	
+	document.getElementById('position').style.display="none";
+	document.getElementById('firstsam').style.display="block";
+	document.getElementById('secondsam').style.display="none";
+}
